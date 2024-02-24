0b1fbb3aa35e2c2d29300b3a75ce7babf2499e1b
Walmyr <wlsf82@gmail.com>
2024-02-24 03:05:41 +0100

    Small greeting adjustment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -106,7 +106,7 @@ const CustomerApp = () => {
         </div>
       ) : (
         <div data-testid="table" className="table-container">
-          <p>Hi <b>{name ? name : 'there'}</b>. It is now <b>{timestamp}</b>.</p>
+          <p>Hi <b>{name ? name : 'there'}</b>! It is now <b>{timestamp}</b>.</p>
           <div>
             <p>Below is our customer list.</p>
             <p>Click on each of them to view their contact details.</p>
