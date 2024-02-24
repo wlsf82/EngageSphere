dcb75dd6e908f14dba7d94f49f53c2227b865284
Walmyr <wlsf82@gmail.com>
2024-02-24 18:53:32 +0100

    Minor frontend fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -145,7 +145,7 @@ const CustomerApp = () => {
                   <path d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path>
                 </svg>
               </div>
-              <div class="no-customers-available-text">
+              <div className="no-customers-available-text">
                 <span>No customers available.</span>
               </div>
             </>
