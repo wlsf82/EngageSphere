e2d7bf299229e15b1309aea163a6e39a44819b1d
Walmyr <wlsf82@gmail.com>
2024-02-29 10:34:28 +0100

    Componentize the empty state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 
+import EmptySate from './components/EmptyState'
 import Footer from './components/Footer'
 import Pagination from './components/Pagination'
 
@@ -162,17 +163,7 @@ const CustomerApp = () => {
       ) : (
         <div data-testid="table" className="table-container">
           {initialFetchDone && !customers.length ? (
-            <>
-              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="400" height="400" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-inbox">
-                  <polyline points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline>
-                  <path d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path>
-                </svg>
-              </div>
-              <div className="no-customers-available-text">
-                <span>No customers available.</span>
-              </div>
-            </>
+            <EmptySate />
           ) : customers.length ? (
             <>
               <p>Hi <b>{name ? name : 'there'}</b>! It is now <b>{(new Date()).toDateString()}</b>.</p>
