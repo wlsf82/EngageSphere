2c5587c097fc918f458d4f8ef0436a87b6ad1ec1
Walmyr <wlsf82@gmail.com>
2024-03-01 22:49:35 +0100

    Small footer adjustments

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,8 +2,8 @@ const Footer = () => {
   return (
     <footer data-testid="footer" className="footer">
       <p>Copyright 2024 - Talking About Testing</p>
-      <a href="https://www.udemy.com/user/walmyr/" target="_blank" rel="noopener noreferrer">Udemy</a>
-      <a href="https://talkingabouttesting.com/" target="_blank" rel="noopener noreferrer">Blog</a>
+      <a href="https://udemy.com/user/walmyr" target="_blank" rel="noopener noreferrer">Udemy</a>
+      <a href="https://talkingabouttesting.com" target="_blank" rel="noopener noreferrer">Blog</a>
       <a href="https://youtube.com/@talkingabouttesting" target="_blank" rel="noopener noreferrer">YouTube</a>
     </footer>
   )
