4b33035538afc3b95d33cd38ad9661b5025bb712
Walmyr <wlsf82@gmail.com>
2024-03-01 18:42:17 +0100

    Add a couple more component tests

---
+++ frontend/src/components/CustomerDetails.cy.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import CustomerDetails from './CustomerDetails'
+
+describe('<CustomerDetails />', () => {
+  let backButtonClickHandler
+
+  beforeEach(() => {
+    backButtonClickHandler = cy.stub().as('backBtnClickHandler')
+  })
+
+  it('renders with contact details', () => {
+    const customer = {
+      name: 'Caca Cala',
+      employees: 1000,
+      size: 'Medium',
+      contactInfo: {
+        name: 'Joe',
+        email: 'joe@cacacala.com'
+      }
+    }
+    cy.mount(<CustomerDetails customer={customer} onClick={backButtonClickHandler} />)
+
+    cy.contains('h2', 'Customer Details').should('be.visible')
+    cy.contains('p', 'Company name: Caca Cala').should('be.visible')
+    cy.contains('p', 'Number of employees: 1000').should('be.visible')
+    cy.contains('p', 'Size: Medium').should('be.visible')
+
+    cy.contains('p', 'Contact name: Joe').should('be.visible')
+    cy.contains('p', 'Contact email: joe@cacacala.com').should('be.visible')
+
+    cy.contains('button', 'Back').should('be.visible')
+  })
+
+  it('renders without contact details', () => {
+    const customer = {
+      name: 'Cocoa Cola',
+      employees: 100,
+      size: 'Small',
+      contactInfo: null
+    }
+    cy.mount(<CustomerDetails customer={customer} onClick={backButtonClickHandler} />)
+
+    cy.contains('h2', 'Customer Details').should('be.visible')
+    cy.contains('p', 'Company name: Cocoa Cola').should('be.visible')
+    cy.contains('p', 'Number of employees: 100').should('be.visible')
+    cy.contains('p', 'Size: Small').should('be.visible')
+
+    cy.contains('p', 'No contact info available').should('be.visible')
+
+    cy.contains('button', 'Back').should('be.visible')
+  })
+})
