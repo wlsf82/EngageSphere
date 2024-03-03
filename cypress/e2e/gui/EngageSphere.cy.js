4576ff56ff648fe15e63b5e3460d4a4373107e75
Walmyr <wlsf82@gmail.com>
2024-03-03 10:02:16 +0100

    Add missing GUI tests

--- cypress/e2e/gui/EngageSphere.cy.js
+++ cypress/e2e/gui/EngageSphere.cy.js
@@ -95,6 +95,164 @@ describe('EngageSphere Frontend', {
     })
   })
 
+  context('Filtering', () => {
+    it('Filters by All sizes', () => {
+      cy.get('tbody tr').should('have.length', 5)
+      cy.get('tbody tr')
+        .eq(0)
+        .find('td')
+        .eq(3)
+        .should('contain', 'Big')
+      cy.get('tbody tr')
+        .eq(1)
+        .find('td')
+        .eq(3)
+        .should('contain', 'Big')
+      cy.get('tbody tr')
+        .eq(2)
+        .find('td')
+        .eq(3)
+        .should('contain', 'Medium')
+      cy.get('tbody tr')
+        .eq(3)
+        .find('td')
+        .eq(3)
+        .should('contain', 'Small')
+      cy.get('tbody tr')
+        .eq(4)
+        .find('td')
+        .eq(3)
+        .should('contain', 'Small')
+    })
+
+    it('Filters by Small size', () => {
+      cy.intercept(
+        'GET',
+        `${Cypress.env('API_URL')}/customers?page=1&limit=10&size=Small`,
+        { fixture: 'smallCustomers' }
+      ).as('getSmallCustomers')
+
+      cy.get('[data-testid="filter"]').select('Small')
+
+      cy.get('tbody tr').should('have.length', 2)
+      cy.get('tbody tr')
+        .first()
+        .find('td')
+        .eq(3)
+        .should('contain', 'Small')
+      cy.get('tbody tr')
+        .last()
+        .find('td')
+        .eq(3)
+        .should('contain', 'Small')
+    })
+
+    it('Filters by Medium size', () => {
+      cy.intercept(
+        'GET',
+        `${Cypress.env('API_URL')}/customers?page=1&limit=10&size=Medium`,
+        { fixture: 'mediumCustomers' }
+      ).as('getMediumCustomers')
+
+      cy.get('[data-testid="filter"]').select('Medium')
+
+      cy.get('tbody tr').should('have.length', 1)
+      cy.get('tbody tr')
+        .find('td')
+        .eq(3)
+        .should('contain', 'Medium')
+    })
+
+    it('Filters by Big size', () => {
+      cy.intercept(
+        'GET',
+        `${Cypress.env('API_URL')}/customers?page=1&limit=10&size=Big`,
+        { fixture: 'bigCustomers' }
+      ).as('getBigCustomers')
+
+      cy.get('[data-testid="filter"]').select('Big')
+
+      cy.get('tbody tr').should('have.length', 2)
+      cy.get('tbody tr')
+        .first()
+        .find('td')
+        .eq(3)
+        .should('contain', 'Big')
+      cy.get('tbody tr')
+        .last()
+        .find('td')
+        .eq(3)
+        .should('contain', 'Big')
+    })
+  })
+
+  context('Pagination', () => {
+    it('changing the page limit is persisted in the local storage', () => {
+      cy.intercept(
+        'GET',
+        `${Cypress.env('API_URL')}/customers?page=1&limit=50**`,
+        { fixture: 'moreCustomers' }
+      ).as('getMoreCustomers')
+
+      cy.get('select[aria-label="Pagination limit"]')
+        .select('50')
+
+      cy.getAllLocalStorage()
+        .then((result) => {
+          const limit = result[Cypress.config('baseUrl')].paginationLimit
+          expect(limit).to.equal('50')
+        })
+
+      cy.reload()
+
+      cy.get('select[aria-label="Pagination limit"]')
+        .should('have.value', 50)
+    })
+  })
+
+  context('Customer details', () => {
+    it('shows the contact info of a specific customer', () => {
+      cy.get('tbody tr')
+        .last()
+        .click()
+
+      cy.contains('p', 'Contact name: Brando')
+        .should('be.visible')
+      cy.contains('p', 'Contact email: Brando_Kozey48@gmail.com')
+        .should('be.visible')
+    })
+
+    it('shows "No contact info available" for a customer without contact information', () => {
+      cy.get('tbody tr')
+        .first()
+        .click()
+
+      cy.contains('p', 'No contact info available')
+        .should('be.visible')
+    })
+
+    it('disables the text input field when in the customer details page', () => {
+      cy.get('tbody tr')
+        .first()
+        .click()
+
+      cy.get('input[type="text"]').should('be.disabled')
+    })
+
+    it('goes back to the customers list when clicking the "Back" button', () => {
+      cy.get('tbody tr')
+        .first()
+        .click()
+
+      cy.get('.customer-details').should('be.visible')
+      cy.get('table').should('not.exist')
+
+      cy.contains('button', 'Back').click()
+
+      cy.get('table').should('be.visible')
+    })
+  })
+
   context('Theme', () => {
     it('changes to the dark mode then back to light mode', () => {
       cy.get('#theme-toggle-button').click()
@@ -113,5 +271,30 @@ describe('EngageSphere Frontend', {
           expect(theme).to.equal('light')
         })
     })
+  })
+})
+
+describe('EngageSphere Frontend - empty state', {
+  viewportHeight: 1240,
+  viewportWidth: 1024
+ }, () => {
+  beforeEach(() => {
+    cy.intercept(
+      'GET',
+      `${Cypress.env('API_URL')}/customers**`,
+      { body: '' }
+    ).as('getEmptyCustomers')
+
+    cy.visit('/')
+    cy.wait('@getEmptyCustomers')
+  })
+
+  it('shows the image of an empty box and the text "No customers available." when there are no customers in the database', () => {
+    cy.get('svg').should('be.visible')
+    cy.contains('span', 'No customers available.').should('be.visible')
+   })
+
+  it('disables the text input field when there are no customers in the database', () => {
+    cy.get('input[type="text"]').should('be.disabled')
   })
 })
