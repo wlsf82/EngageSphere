db825ab244052e01417e47e8e1c968c9087882a6
Walmyr <wlsf82@gmail.com>
2024-03-06 00:29:17 +0100

    Fix warning on component test

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -2,7 +2,7 @@ import './commands'
 
 import '../../frontend/src/index.css'
 
-import { mount } from 'cypress/react'
+import { mount } from 'cypress/react18'
 
 Cypress.Commands.add('mount', mount)
 
