bc6f0336f7f663dd87393a8a85326198514b0413
Walmyr <wlsf82@gmail.com>
2024-05-23 02:54:26 +0200

    Update fixtures with at least one complete customer
    Where a complete customer means a customer with id, name, employess,
    size, contact details, and address info.

--- cypress/fixtures/sampleCSV.js
+++ cypress/fixtures/sampleCSV.js
@@ -1,6 +1,6 @@
 const csvSample = `ID,Company_Name,Number_of_Employees,Size,Contact_Name,Contact_Email,Street,City,State,Zip_Code,Country
 "1","Jacobs, Bechtelar and Von","2174","Enterprise","","","","","","",""
-"2","Kilback - Kerluke","226","Medium","Daija","Daija_Gislason93@gmail.com","","","","",""
+"2","Kilback - Kerluke","226","Medium","Daija","Daija_Gislason93@gmail.com","988 Kimberly Fort Apt. 921","Lake Tracy","Connecticut","07115","United States of America"
 "3","Parisian - Berge","47","Small","Alysson","Alysson.Lang@hotmail.com","","","","",""
 "4","Wilderman, Marks and Funk","24000","Large Enterprise","Brando","Brando_Kozey48@gmail.com","","","","",""
 "5","Runolfsson, Satterfield and Huel","51015","Very Large Enterprise","","","","","","",""`
