403b7b92e632c88230cfcacd7a4a32ed16f1f587
Walmyr <wlsf82@gmail.com>
2024-07-04 19:52:16 +0200

    Make test pass

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,6 +2,7 @@ const Footer = () => {
   return (
     <footer data-testid="footer">
       <p>Copyright 2024 - Talking About Testing</p>
+      <a href="https://hotmart.com/pt-br/club/talking-about-testing" target="_blank" rel="noopener noreferrer">Hotmart</a>
       <a href="https://udemy.com/user/walmyr" target="_blank" rel="noopener noreferrer">Udemy</a>
       <a href="https://talkingabouttesting.com" target="_blank" rel="noopener noreferrer">Blog</a>
       <a href="https://youtube.com/@talkingabouttesting" target="_blank" rel="noopener noreferrer">YouTube</a>
