681f9eeb18fd004e4900f4194688db51c01ee0f9
Walmyr <wlsf82@gmail.com>
2024-07-04 19:51:16 +0200

    Start a TDD cycle with a failing test

--- frontend/src/components/Footer.cy.js
+++ frontend/src/components/Footer.cy.js
@@ -6,6 +6,9 @@ describe('<Footer />', () => {
 
     cy.contains('p', 'Copyright 2024 - Talking About Testing')
       .should('be.visible')
+    cy.contains('a', 'Hotmart')
+      .should('be.visible')
+      .and('have.attr', 'href', 'https://hotmart.com/pt-br/club/talking-about-testing')
     cy.contains('a', 'Udemy')
       .should('be.visible')
       .and('have.attr', 'href', 'https://udemy.com/user/walmyr')
