335d5b66142639113c5e6686b313c7710c6757d2
Walmyr <wlsf82@gmail.com>
2024-08-19 01:41:18 +0200

    Update frontend to use StrictMode
    Use StrictMode to enable additional development behaviors and
    warnings for the component tree inside.
    
    Source: https://react.dev/reference/react/StrictMode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
@@ -6,7 +6,11 @@ import * as serviceWorker from './serviceWorker'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
-root.render(<App />)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
