5560ee51884f9b0922cfac99454a9b087b6fa47d
Walmyr <wlsf82@gmail.com>
2024-09-12 17:03:49 +0200

    Adjust the app with a cleaner style

--- frontend/src/components/Greeting/index.js
+++ frontend/src/components/Greeting/index.js
@@ -1,12 +1,14 @@
+import styles from './Greeting.module.css'
+
 const Greeting = ({ name }) => {
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
   const today = new Date()
   return (
-    <>
-      <p>Hi <b>{name || 'there'}</b>! It is <b>{today.toLocaleDateString("en-US", options)}</b>.</p>
+    <div className={styles.container}>
+      <h2>Hi {name || 'there'}! It is {today.toLocaleDateString("en-US", options)}.</h2>
       <p>Below is our customer list.</p>
       <p>Click on the <strong>View</strong> button of each of them to see their contact details.</p>
-    </>
+    </div>
   )
 }
 
