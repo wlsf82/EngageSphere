b59d5d970033be66d1c07676cfbb8a56d35c0618
Walmyr <wlsf82@gmail.com>
2024-09-15 03:06:44 +0200

    Add an easteregg on the greeting component

--- frontend/src/components/Greeting/index.js
+++ frontend/src/components/Greeting/index.js
@@ -1,9 +1,11 @@
+import { Squirrel } from 'lucide-react'
+
 const Greeting = ({ name }) => {
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
   const today = new Date()
   return (
     <>
-      <h2>Hi {name || 'there'}! It is {today.toLocaleDateString("en-US", options)}</h2>
+      <h2>Hi {name || 'there'}! {name === 'Squirrel' ? <Squirrel /> : null} It is {today.toLocaleDateString("en-US", options)}</h2>
       <p>Below is our customer list.</p>
       <p>Click on the <strong>View</strong> button of each of them to see their contact details.</p>
     </>
