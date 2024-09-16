204c6049c176721bcad2535f61634426efb33019
Walmyr <wlsf82@gmail.com>
2024-09-16 17:18:58 +0200

    Add defaults for button component props

--- frontend/src/components/Button/index.js
+++ frontend/src/components/Button/index.js
@@ -1,6 +1,10 @@
 import styles from './Button.module.css'
 
-const Button = ({ icon = null, text, onClick }) => {
+const Button = ({
+  icon = null,
+  text = null,
+  onClick = () => {}
+}) => {
   return (
     <div className={styles.buttonContainer}>
       <button className={styles.button} onClick={onClick}>
