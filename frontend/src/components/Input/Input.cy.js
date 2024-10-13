9d26aface8902a1b4df75dad42e8feae413d9e99
Walmyr <wlsf82@gmail.com>
2024-10-13 21:43:34 +0200

    Start a TDD cycle - status: red

--- frontend/src/components/Input/Input.cy.js
+++ frontend/src/components/Input/Input.cy.js
@@ -13,4 +13,11 @@ describe('<Input />', () => {
 
     cy.get('input[placeholder="E.g., John Doe"]').should('be.disabled')
   })
+
+  it('limits the input data to 40 characters', () => {
+    cy.mount(<Input disabled={false} />)
+
+    cy.get('input[placeholder="E.g., John Doe"]')
+      .should('have.attr', 'maxlength', '40')
+  })
 })
